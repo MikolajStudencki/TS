c583753132ea7d504ea4a1767cc8837b694ec195
Danylon <skrahlenkod@gmail.com>
2023-05-26 13:08:53 +0200

    remade blink method

--- Core/Src/change_datetime_screen.c
+++ Core/Src/change_datetime_screen.c
@@ -1,13 +1,16 @@
 #include "change_datetime_screen.h"
 
+typedef void (*DisplayFunction)(Lcd_HandleTypeDef*);
+
 static void displayDate(Lcd_HandleTypeDef *lcd);
 static void displayTime(Lcd_HandleTypeDef *lcd);
 static void displayArrowUp(Lcd_HandleTypeDef *lcd);
 static void displayArrowDown(Lcd_HandleTypeDef *lcd);
-static void blink(Lcd_HandleTypeDef *lcd, uint8_t col_var, uint8_t row);
+static void blink(Lcd_HandleTypeDef *lcd, uint8_t row, uint8_t col, uint32_t length, DisplayFunction display_var);
 
 static uint32_t end_time;
 static uint32_t start_time;
+static uint8_t counter = 0;
 
 //static uint8_t selectedIndexX;
 //static uint8_t selectedIndexY;
@@ -17,24 +20,46 @@ void displayChangeDateTimeScreen(Lcd_HandleTypeDef *lcd)
 {
 	displayArrowUp(lcd);
 	displayArrowDown(lcd);
-	blink(lcd, 11, 0);
-	Lcd_cursor(lcd, 0, 0);
-	displayDate(lcd);
+	blink(lcd, 0, 1, 11, displayDate);
 	displayTime(lcd);
 }
 
-static void blink(Lcd_HandleTypeDef *lcd, uint8_t col_var, uint8_t row)
+static void blink(Lcd_HandleTypeDef *lcd, uint8_t row, uint8_t col, uint32_t length, DisplayFunction display_var)
 {
+	char emptyCharArray[length];
 	end_time = HAL_GetTick();
 
-	if(end_time - start_time >= 100)
+	for (int i = 0; i < length; i++)
 	{
-		for(uint8_t col = 1;col<col_var;col++)
+		emptyCharArray[i] = ' ';
+	}
+	emptyCharArray[length] = '\0';
+
+	if(end_time - start_time >= 1000)
+	{
+		switch(counter)
+		{
+			case 0:
+			{
+				display_var(lcd);
+				start_time = end_time;
+				counter++;
+				break;
+			}
+			case 3:
+			{
+				counter = 0;
+				break;
+			}
+			default:
 			{
 				Lcd_cursor(lcd, row, col);
-				Lcd_string(lcd," ");
+				Lcd_string(lcd, emptyCharArray);
+				counter++;
+				break;
 			}
-		start_time = end_time;
+		}
+
 	}
 }
 
